8209608b7eff2797b34dbcd0a6b6c255d8cc63ee
HundOne2230 <45989649+HundOne2230@users.noreply.github.com>
2019-05-23 17:03:46 +0900

    jumptomap
    maprahu

---
+++ Assets/jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+
+    bool isJump = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)&& isJump == true)
+        {
+            transform.GetComponent<Rigidbody>().AddForce(0, 5, 0, ForceMode.Impulse);
+            isJump = false;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isJump = true;
+        }
+    }
+}
