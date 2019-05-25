fa40d5bc0eb45688f1f0550f5af0d7b1958d0665
Masamune1192 <akjru2502@gmail.com>
2019-05-26 00:52:27 +0900

    cameraview
    視点移動のスクリプトを追加

---
+++ Assets/look.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class look : MonoBehaviour
+{
+
+    public Transform playertrans;
+    public Transform cameratrans;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playertrans = transform.parent;
+        cameratrans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X_Rotation = Input.GetAxis("Mouse X");
+        float Y_Rotation = Input.GetAxis("Mouse Y");
+        playertrans.transform.Rotate(0, -X_Rotation, 0);
+        cameratrans.transform.Rotate(Y_Rotation, 0, 0);
+    }
+}
