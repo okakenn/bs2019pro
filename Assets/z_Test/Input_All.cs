076ebebd6b6830fb662de8603a92c47f6d408d57
okakenn <dog.okamura@gmail.com>
2019-05-30 18:05:01 +0900

    疲れた
    休みほしい

---
+++ Assets/z_Test/Input_All.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_All : MonoBehaviour {
+
+    public bool GO_FRONT, GO_BACK, GO_RIGHT, GO_LEFT;
+    public bool DO_JUMP;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //1.1前後左右の移動、ジャンプの入力
+        //
+        //
+        //
+
+        //1.1前後左右の移動、ジャンプ
+        if (Input.GetKey(KeyCode.W))
+        {
+            GO_FRONT = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            GO_FRONT = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GO_LEFT = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            GO_LEFT = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            GO_BACK = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            GO_BACK = false;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GO_RIGHT = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            GO_RIGHT = false;
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            DO_JUMP = true;
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            DO_JUMP = false;
+        }
+
+
+        //GO_FRONT = false;
+        //GO_BACK = false;
+        //GO_RIGHT = false;
+        //GO_LEFT = false;
+        //DO_JUMP = false;
+
+
+    }
+}
---
+++ Assets/z_Test/Player_move.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour {
+
+    //キー入力のための変数１
+    private Input_All INPUT_All;
+    public GameObject GAME_CONTROL;
+    bool G_FRONT,G_LEFT,G_BACK,G_RIGHT;
+    bool D_JUMP;
+    ///////////////////////////
+
+    ///速度の変数
+    public float SPEED = 5.0f;
+    public float PTURN_SPEED = 0.5f;
+    ///
+
+    ///移動関係
+    Vector3 HOUKOU;
+    ///
+
+    ///
+    public GameObject MAIN_CAMERA;
+    Camera_move C_MOVE;
+    ///
+
+	// Use this for initialization
+	void Start () {
+
+        //キー入力のための変数２
+        G_FRONT = GAME_CONTROL.GetComponent<Input_All>().GO_FRONT;
+        G_LEFT = GAME_CONTROL.GetComponent<Input_All>().GO_LEFT;
+        G_BACK = GAME_CONTROL.GetComponent<Input_All>().GO_BACK;
+        G_RIGHT = GAME_CONTROL.GetComponent<Input_All>().GO_RIGHT;
+        D_JUMP = GAME_CONTROL.GetComponent<Input_All>().DO_JUMP;
+        ///////////////////////////////////
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //キー入力のための変数２
+        G_FRONT = GAME_CONTROL.GetComponent<Input_All>().GO_FRONT;
+        G_LEFT = GAME_CONTROL.GetComponent<Input_All>().GO_LEFT;
+        G_BACK = GAME_CONTROL.GetComponent<Input_All>().GO_BACK;
+        G_RIGHT = GAME_CONTROL.GetComponent<Input_All>().GO_RIGHT;
+        D_JUMP = GAME_CONTROL.GetComponent<Input_All>().DO_JUMP;
+        ///////////////////////////////////
+
+        ///
+        C_MOVE = MAIN_CAMERA.GetComponent<Camera_move>();
+
+        HOUKOU = Vector3.zero;
+        if (G_FRONT)
+        {
+            HOUKOU.z += 1.0f;
+        }
+        Debug.Log(G_FRONT);
+
+        if (G_RIGHT)
+        {
+            HOUKOU.x += 1.0f;
+        }
+        if (G_BACK)
+        {
+            HOUKOU.z -= 1.0f;
+        }
+        if (G_LEFT)
+        {
+            HOUKOU.x -= 1.0f;
+        }
+
+        HOUKOU = HOUKOU.normalized * SPEED * Time.deltaTime;
+
+        if (HOUKOU.magnitude > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(C_MOVE.ROTATION_H*HOUKOU), PTURN_SPEED);
+
+            transform.position += C_MOVE.ROTATION_H * HOUKOU;
+
+        }
+
+        //Rigidbody PLAYER_RB = this.GetComponent<Rigidbody>();
+        //Vector3 FRONT = new Vector3(0f, 0f, 0f);
+
+
+
+	}
+}
